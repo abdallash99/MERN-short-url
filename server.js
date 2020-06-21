1a8fa30512838e873b48d3c002676f256df8d32d
abdallash <abd0173435@gmail.com>
2020-06-21 15:44:58 +0300

    adding production

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ app.use(express.json({ extended: false }))
 
 app.use('/', require('./routes/index'))
 app.use('/api/url', require('./routes/short'))
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
 
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
 const PORT = process.env.PORT || 5000
 
